43ed260bd09b5a3e514aa12761219c59b48a3e14
mattyshalom <olusegesunday@yahoo.com>
2023-08-26 08:25:20 +0100

    updated

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+/*filter tab*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('filters__active')
+        })
+        target.classList.add('filters__active')
+    })
+})
